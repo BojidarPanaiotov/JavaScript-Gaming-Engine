86dd7ce4d67fb7ea044dd8d25318400e723d51dd
62803702+BojidarPanaiotov@users.noreply.github.com <62803702+BojidarPanaiotov@users.noreply.github.com>
2026-08-21 17:22:51 +0300

    Introduce a function to start the Game object

--- game/classes/Game.js
+++ game/classes/Game.js
@@ -1,14 +1,19 @@
 export class Game {
   #canvasId = 'canvas-game-screen';
+  width;
+  height;
 
   constructor(width, height) {
-    this.canvas = document.getElementById(this.#canvasId);
-    this.canvas.width = width;
-    this.canvas.height = height;
-    this.ctx = this.canvas.getContext('2d');
+    this.width = width;
+    this.height = height;
   }
 
   start() {
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    const canvasElement = document.createElement('canvas');
+    canvasElement.id = this.#canvasId;
+    canvasElement.width = this.width;
+    canvasElement.height = this.height;
+    canvasElement.style.border = '1px solid red';
+    document.body.appendChild(canvasElement);
   }
 }
