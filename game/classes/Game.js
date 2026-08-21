8ba3b3e0e9c4cf9dc42dc1d9c61cbca35d547aa3
62803702+BojidarPanaiotov@users.noreply.github.com <62803702+BojidarPanaiotov@users.noreply.github.com>
2026-08-21 17:45:13 +0300

    Add game parameters for fullscreen and border

--- game/classes/Game.js
+++ game/classes/Game.js
@@ -1,19 +1,44 @@
 export class Game {
   #canvasId = 'canvas-game-screen';
+  #borderWidth = 1;
   width;
   height;
+  fullScreen;
+  spacing;
+  showBorder;
 
-  constructor(width, height) {
+  constructor(width, height, fullScreen = false, spacing = 0, showBorder = false) {
     this.width = width;
     this.height = height;
+    this.fullScreen = fullScreen;
+    this.spacing = spacing;
+    this.showBorder = showBorder;
   }
 
   start() {
     const canvasElement = document.createElement('canvas');
     canvasElement.id = this.#canvasId;
-    canvasElement.width = this.width;
-    canvasElement.height = this.height;
-    canvasElement.style.border = '1px solid red';
+
+    if (this.showBorder) {
+      canvasElement.style.border = `${this.#borderWidth}px solid red`;
+    }
+  
+    if (this.fullScreen) {
+      let minusWidth = this.spacing * 2;
+      let minusHeight = this.spacing * 2;
+
+      if (this.showBorder) {
+        minusWidth += this.#borderWidth * 2;
+        minusHeight += this.#borderWidth * 2;
+      }
+
+      canvasElement.style.margin = `${this.spacing}px`;
+      canvasElement.width = window.innerWidth - minusWidth;
+      canvasElement.height = window.innerHeight - minusHeight;
+    } else {
+      canvasElement.width = this.width;
+      canvasElement.height = this.height;
+    }
     document.body.appendChild(canvasElement);
   }
 }
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import { Game } from './game/classes/Game.js';
 
-const game = new Game(600, 400);
+const game = new Game(400, 400, true, 50, true);
 game.start();
