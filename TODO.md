4f784ed4758f52265857eb490663aac9952dcf89
62803702+BojidarPanaiotov@users.noreply.github.com <62803702+BojidarPanaiotov@users.noreply.github.com>
2026-08-25 11:43:32 +0300

    Add a parameter for applying space onto the canvas outer part

--- TODO.md
+++ TODO.md
@@ -5,4 +5,5 @@
 - [x] Add abstract class for the main game object
 - [x] Implement the class and draw the canvas onto the screen
 - [x] Make canvas fullscreen (optional)
-- [ ] Make canvas set optionl width and height plus border
+- [x] Make canvas set optionl width and height plus border
+- [ ] Set configurable outer space for canva
--- game/entities/GameMap.ts
+++ game/entities/GameMap.ts
@@ -4,24 +4,36 @@ export abstract class GameMap {
   canvas: HTMLCanvasElement;
 
   constructor(
-    width?: number, 
-    height?: number, 
+    width: number = 800, 
+    height: number = 400, 
     fullscreen: boolean = true, 
-    border: boolean = true
+    border: boolean = true,
+    spacing: number = 16
   ) {
     const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
 
     if (border) {
       canvas.style.border = `${GAME.SCREEN_BORDER_WIDTH}px solid ${GAME.SCREEN_BORDER_COLOR}`;
     }
 
+    if (spacing) {
+      canvas.style.margin = `${spacing}px`;
+    }
+
     if (fullscreen) {
-      canvas.width = width || innerWidth;
-      canvas.height = height || innerHeight;
+      canvas.width = innerWidth;
+      canvas.height = innerHeight;
 
       if (border) {
         canvas.width = canvas.width - (GAME.SCREEN_BORDER_WIDTH * 2);
         canvas.height = canvas.height - (GAME.SCREEN_BORDER_WIDTH * 2);
+      }
+
+      if (spacing) {
+        canvas.width = canvas.width - spacing * 2;
+        canvas.height = canvas.height - spacing * 2;
       }
     }
 
