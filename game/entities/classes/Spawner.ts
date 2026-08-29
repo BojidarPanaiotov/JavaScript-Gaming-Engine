4f4ceee9c512ddf29aa55cf0abfbb7d04f6ca1b2
62803702+BojidarPanaiotov@users.noreply.github.com <62803702+BojidarPanaiotov@users.noreply.github.com>
2026-08-29 15:16:28 +0300

    Introduce Spawner Class

--- game/entities/classes/Spawner.ts
+++ game/entities/classes/Spawner.ts
@@ -1,4 +1,45 @@
+import { generateRandomNumber } from "../../utils/algorithms/utils";
 import { GameObject } from "../abstraction/GameObject";
+import { SpriteSheet } from "./SpriteSheet";
 
-export class Spawner <T extends GameObject> {
+export type GameObjectCtor<T extends GameObject> = new (
+  x: number,
+  y: number,
+  size: number,
+  spriteSheet?: SpriteSheet,
+) => T;
+
+export class Spawner<T extends GameObject> {
+  #spawnedObjects: T[] = [];
+
+  constructor(
+    private readonly Ctor: GameObjectCtor<T>,
+    private readonly size: number,
+    private readonly spriteSheet?: SpriteSheet,
+    count: number = 1
+  ) {
+    this.#spawnedObjects = this.#create(count);
+  }
+
+  spawn(): void {
+    this.#render();
+  }
+
+  #create(count: number): T[] {
+    const spawned: T[] = [];
+
+    for (let i = 0; i < count; i++) {
+      const x = generateRandomNumber(0, game.canvas.width - this.size);
+      const y = generateRandomNumber(0, game.canvas.height - this.size);
+      spawned.push(new this.Ctor(x, y, this.size, this.spriteSheet));
+    }
+
+    return spawned;
+  }
+
+  #render(): void {
+    this.#spawnedObjects.forEach(object => {
+      object.render();
+    });
+  }
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 import { SpriteSheet } from "./game/entities/classes/SpriteSheet";
 import { Game } from "./game/entities/classes/Game";
 import { Dino } from "./game/entities/classes/Dino";
-import { Controller } from "./game/entities/classes/Controller";
+import { Spawner } from "./game/entities/classes/Spawner";
+import { generateRandomNumber } from "./game/utils/algorithms/utils";
 
 const game = new Game();
 globalThis.game = game;
@@ -10,25 +11,16 @@ game.start();
 const speed = 5;
 const size = 96;
 const dinoSpriteSheet = new SpriteSheet('./game/assets/dino-walk.png', true);
-const dino = new Dino(0, 0, size, dinoSpriteSheet);
-const dino2 = new Dino(100, 100, size, dinoSpriteSheet);
-const controller = new Controller(dino);
+const spawner = new Spawner(Dino, size, dinoSpriteSheet);
 
 function loop() {
-  controller.activate(speed);
   game.clear();
-
-  dino.render();
-  dino.collider.renderBorder();
-  dino2.render();
-  dino2.collider.renderBorder();
-
-  const collides = dino.collider.collideSingle(dino2);
-  if (collides) {
-    console.log('Collides');
-  }
-
+  spawner.spawn();
+  game.gameObjects.forEach(gameObject => {
+    gameObject.collider.renderBorder();
+  });
   requestAnimationFrame(loop);
 }
+
 
 requestAnimationFrame(loop);
