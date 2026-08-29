003401ae4b422901cef9dfa190a59e72bacd7bb6
62803702+BojidarPanaiotov@users.noreply.github.com <62803702+BojidarPanaiotov@users.noreply.github.com>
2026-08-29 17:14:55 +0300

    Remove main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import { Game } from './game/classes/Game.js';
-import { Square } from './game/classes/Square.js';
-
-const game = new Game(400, 400, true, 20, true);
-game.start();
-
-const square = new Square(100, 100, 80, game.ctx);
-game.render(square);
