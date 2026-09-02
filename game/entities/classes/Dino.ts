06ea9d76e96a58eb1d3fce3a5a70d25d5831df72
62803702+BojidarPanaiotov@users.noreply.github.com <62803702+BojidarPanaiotov@users.noreply.github.com>
2026-09-02 15:49:57 +0300

    Make the code more readable

--- game/entities/classes/Dino.ts
+++ game/entities/classes/Dino.ts
@@ -5,7 +5,10 @@ export class Dino extends AnimatedGameObject {
   public itemCollection: IGameObject[] = [];
 
   update(x: number, y: number): void {
+    const isMoving = x !== 0 || y !== 0;
+
     super.update(x, y);
-    this.currentAnimation = x !== 0 || y !== 0 ? "walk" : "idle";
+  
+    this.currentAnimation = isMoving ? "walk" : "idle";
   }
 }
