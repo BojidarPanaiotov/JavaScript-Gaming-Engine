16f86cdb4126ff4e004f631c68672cc2451a603c
62803702+BojidarPanaiotov@users.noreply.github.com <62803702+BojidarPanaiotov@users.noreply.github.com>
2026-09-02 14:01:45 +0300

    Introduce Weapon class

---
+++ game/entities/classes/items/Weapon.ts
@@ -0,0 +1,21 @@
+import { AnimatedGameObject } from "../../abstraction/gameObject/AnimatedGameObject";
+import { ISpriteSheet } from "../SpriteSheet";
+import { Animations } from "../../abstraction/gameObject/AnimatedGameObject";
+
+export class Pistol extends AnimatedGameObject {
+  public totalAmmo: number = 0;
+  
+  constructor(
+    x: number, 
+    y: number, 
+    width: number, 
+    height: number, 
+    animations: Animations,
+    spriteSheet: ISpriteSheet,
+    totalAmmo: number,
+    collider?: boolean
+  ) {
+    super(x, y, width, height, animations, spriteSheet, collider);
+    this.totalAmmo = totalAmmo;
+  }
+}
