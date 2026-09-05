2e83694fc6ad9863a9831cfb634bf49754685384
BojidarPanaiotov <bojidar.panaiotov.panaiotov@gmail.com>
2026-09-05 13:59:45 +0300

    Change health color based on object health

--- game/entities/interfaces/Health.ts
+++ game/entities/interfaces/Health.ts
@@ -13,14 +13,21 @@ obj: Health & { x: number; y: number; width: number }
     const x = obj.x;
     const y = obj.y - barHeight - 10;
     const healthRatio = Math.max(0, Math.min(1, obj.health / obj.maxHealth));
+    let healthColor = "limegreen";
 
+    if (healthRatio < 0.5) {
+        healthColor = "orange";
+    }
+    if (healthRatio < 0.25) {
+        healthColor = "red";
+    }
 
     ctx.save();
     // Color of the background
     ctx.fillStyle = "#333";
     ctx.fillRect(x, y, barWidth, barHeight);
     // Color of the health
-    ctx.fillStyle = "limegreen";
+    ctx.fillStyle = healthColor;
     ctx.fillRect(x, y, barWidth * healthRatio, barHeight);
     // Color of the border
     ctx.strokeStyle = "#111";
