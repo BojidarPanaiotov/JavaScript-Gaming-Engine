ffb32df472b3261b9e743909c68c29f6960c010e
BojidarPanaiotov <bojidar.panaiotov.panaiotov@gmail.com>
2026-09-10 18:37:37 +0300

    Now Controller have single responsibility method which returns the next step,
    Remove game object dependency from Controller

--- game/entities/controls/Controller.ts
+++ game/entities/controls/Controller.ts
@@ -1,16 +1,9 @@
-import { IAnimatedGameObject } from "../gameObject/AnimatedGameObject";
-import { IBaseGameObject } from "../gameObject/BaseGameObject";
-
 export class Controller {
   keys = new Set<string>();
-  gameObjects: IAnimatedGameObject[];
 
-  constructor(gameObject: IAnimatedGameObject);
-  constructor(gameObjects: IAnimatedGameObject[]);
-  constructor(gameObject: IAnimatedGameObject | IAnimatedGameObject[]) {
+  constructor() {
     this.bindKeyDownEvent();
     this.bindKeyUpEvent();
-    this.gameObjects = Array.isArray(gameObject) ? gameObject : [gameObject];
   }
 
   bindKeyDownEvent(): void {
@@ -25,7 +18,7 @@ export class Controller {
     });
   }
 
-  move(speed: number = 1): void {
+  getNextMoveCoordinates(speed: number = 1): {x: number, y: number} {
     let x = 0;
     let y = 0;
 
@@ -42,10 +35,6 @@ export class Controller {
       y += speed;
     }
 
-    const isMoving = x !== 0 || y !== 0;
-
-    this.gameObjects.forEach((gameObject) => {
-      gameObject.update(gameObject.x + x, gameObject.y + y, 0, isMoving ? "walk" : "idle");
-    });
+    return {x, y};
   }
 }
