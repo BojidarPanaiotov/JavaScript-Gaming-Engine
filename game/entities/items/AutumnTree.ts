f89459219bc341d172566711718e11630ad7439c
BojidarPanaiotov <bojidar.panaiotov.panaiotov@gmail.com>
2026-09-11 09:33:33 +0300

    Add a static tree game object

---
+++ game/entities/items/AutumnTree.ts
@@ -0,0 +1,4 @@
+import { AnimatedGameObject } from "../gameObject/AnimatedGameObject";
+
+export class AutumnTree extends AnimatedGameObject {
+}
--- game/utils/init.ts
+++ game/utils/init.ts
@@ -21,6 +21,9 @@ function initAnimationClips() {
     },
     coin: {
       idle: { from: 0, to: 7, fps: 6 }
+    },
+    autumnTree: {
+      idle: { from: 0, to: 15, fps: 10 }
     }
   }
 
@@ -34,6 +37,7 @@ function initSpriteSheets() {
     player: new SpriteSheet('./game/assets/orange-demon', true, 40, 8),
     pistol: new SpriteSheet('./game/assets/pistol', true, 8),
     coin: new SpriteSheet('./game/assets/coin', true, 8),
+    autumnTree: new SpriteSheet('./game/assets/autumn-tree', true, 16, 16),
   }
 
   return spriteSheets;
